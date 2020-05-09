ed8e74f4217949e7c4a1393040704c76a7825b75
Sayantan Chaudhuri <sayantandev2018@gmail.com>
2020-05-09 12:59:40 +0530

    First App

--- src/TryDjango/views.py
+++ src/TryDjango/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def home_page(request):
     my_title = "Hello there..."
-    return render(request, 'hello_world.html', {"title": my_title})
+    return render(request, 'home.html', {"title": my_title})
 
 def about_page(request):
     return render(request, 'about.html', {"title": "About Us"})
