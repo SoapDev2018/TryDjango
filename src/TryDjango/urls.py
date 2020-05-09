ac6a643e18d158a423999b4f918fa2c04f48b6cf
Sayantan Chaudhuri <sayantandev2018@gmail.com>
2020-05-09 15:51:31 +0530

    Updated views

--- src/TryDjango/urls.py
+++ src/TryDjango/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from .views import home_page, about_page, contact_page
 
 urlpatterns = [
     path('', home_page),
     path('about/', about_page),
+    path('blog/', include('blog.urls')),
     path('contact/', contact_page),
     path('admin/', admin.site.urls),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import BlogPost
+
+def blog_post_create_view(request):
+    template_name = 'blog/blog_post_create.html'
+    context = {'form': None}
+    return render(request, template_name, context)
+
+def blog_post_detail_view(request, slug):
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog/blog_post_detail.html'
+    context = {'object': obj}
+    return render(request, template_name, context)
+
+def blog_post_list_view(request):
+    template_name = 'blog/blog_post_list.html'
+    context = {'object_list': []}
+    return render(request, template_name, context)
+
+def blog_post_update_view(request, slug):
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog/blog_post_update.html'
+    context = {'object': obj, 'form': None}
+    return render(request, template_name, context)
+
+def blog_post_delete_view(request, slug):
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog/blog_post_detail.html'
+    context = {'object': obj}
+    return render(request, template_name, context)
