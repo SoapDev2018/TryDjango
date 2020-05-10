725c4e0221b7e0ec6c47ade7125c8b98df339833
Sayantan Chaudhuri <sayantandev2018@gmail.com>
2020-05-10 12:52:59 +0530

    Prettified templates

--- src/blog/views.py
+++ src/blog/views.py
@@ -4,13 +4,13 @@ from django.utils import timezone
 from .models import BlogPost
 
 def blog_post_create_view(request):
-    template_name = 'blog/blog_post_create.html'
+    template_name = 'blog/create.html'
     context = {'form': None}
     return render(request, template_name, context)
 
 def blog_post_detail_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog/blog_post_detail.html'
+    template_name = 'blog/detail.html'
     context = {'object': obj}
     return render(request, template_name, context)
 
@@ -18,18 +18,18 @@ def blog_post_list_view(request):
     # Fetch last 5 published blogposts only
     queryset = BlogPost.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
     print(queryset)
-    template_name = 'blog/blog_post_list.html'
+    template_name = 'blog/list.html'
     context = {'object_list': queryset}
     return render(request, template_name, context)
 
 def blog_post_update_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog/blog_post_update.html'
+    template_name = 'blog/update.html'
     context = {'object': obj, 'form': None}
     return render(request, template_name, context)
 
 def blog_post_delete_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog/blog_post_delete.html'
+    template_name = 'blog/delete.html'
     context = {'object': obj}
     return render(request, template_name, context)
